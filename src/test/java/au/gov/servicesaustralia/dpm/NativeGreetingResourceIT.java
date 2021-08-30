4d20b63397c61abd0df7e9a06af58ff3b8ce5213
Gurdial Singh <gurdial.singh@gmail.com>
2021-08-30 16:22:52 +1000

    initial commit

---
+++ src/test/java/au/gov/servicesaustralia/dpm/NativeGreetingResourceIT.java
@@ -0,0 +1,9 @@
+package au.gov.servicesaustralia.dpm;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeGreetingResourceIT extends QuarkusSortTest {
+
+    // Execute the same tests but in native mode.
+}
---
+++ src/test/java/au/gov/servicesaustralia/dpm/QuarkusSortTest.java
@@ -0,0 +1,20 @@
+package au.gov.servicesaustralia.dpm;
+
+import io.quarkus.test.junit.QuarkusTest;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.is;
+
+@QuarkusTest
+public class QuarkusSortTest {
+
+    @Test
+    public void testHelloEndpoint() {
+        given()
+          .when().get("/hello")
+          .then()
+             .statusCode(200);
+    }
+
+}
